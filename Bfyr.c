9400bf887b3bf6a57c9796ee0b1c87eedeb449ec
manish kakoti <madguy02@users.noreply.github.com>
2015-02-24 18:59:38 +0530

    Create Bfyr.c

---
+++ Bfyr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	long int t,eti,med1,med2,med3,med4,med5;
+	long long int Bfyr;
+	
+	Bfyr = (t+eti)/3600;
+	med1= Bfyr/24;
+	med2= med1/365.25;
+	med3= med2+2000;
+	//printf("the value is %ld",med3);
+	return 0;
+}
+	
